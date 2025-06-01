b7d42d3290664301111ec07e1af6c9c3adc3e776
Sohaup <sohybta560@gmail.com>
2025-06-01 18:53:26 +0300

    assignment ten (Quote of the day )

---
+++ Assignment_ten/js/main.js
@@ -0,0 +1,26 @@
+var quotes = [
+    { quote : "Restment is Like Drinking Poison and waiting for your enemies to die" , author:  "Nelson Mandela" },
+    {  quote : "Do Not Take Life too Seriously You Will Not Get Out A live" , author:"Elbert Hubbard" } ,
+    {  quote : "You Miss 100% Of The Shots You Do Not Take" , author:"Wayne Gretzy"},
+    {  quote : "It`s NOT What happens to you , but how you reat to it that matters" , author:"Epictetus"},
+    {  quote : "The best revenge is massive Success" , author:"Frank Sinatra"}
+];
+
+var h2Container = document.getElementById("quoteCont");
+var previousRandom = null;
+function displayQuote() {    
+    h2Container.innerHTML = "";
+    var randome = Math.random() * quotes.length;    
+    
+    setTimeout(function () {
+        h2Container.innerHTML = 
+        `
+        <h2 class="d-flex flex-column justify-content-center gap-3  ">
+        <span>${quotes[Math.floor(randome)].quote}</span>
+        <span>--${quotes[Math.floor(randome)].author}</span>
+        </h2>
+        
+        `;
+    } , 800)
+  
+}
